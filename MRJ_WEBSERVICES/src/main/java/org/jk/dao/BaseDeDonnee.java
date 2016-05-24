bc89e802e230c51857010afef3f7cea389613e04
RomainRea <rea.romain@gmail.com>
2016-05-24 15:40:28 +0200

    connectionBDD-Train

---
+++ MRJ_WEBSERVICES/src/main/java/org/jk/dao/BaseDeDonnee.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soa.jaxrslabs.booktrainrestwebserviceexcercice2;
+
+/**
+ *
+ * @author Rom1
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class BaseDeDonnee
+{
+   public static void main(String[] args)
+    {
+        try
+        {
+            System.out.println("FUCKING TEST \n");
+            String url = "jdbc:derby://localhost:1527/EpreuveTechHitema";
+            String user = "root";
+            String passwd = "root";
+
+            Connection conn = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connexion effective !");         
+         
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from Train");
+            ResultSetMetaData resultMeta = rs.getMetaData();
+
+            while(rs.next())
+            {         
+            	for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+                System.out.print("\t" + rs.getObject(i).toString() + "\t |");
+
+            	System.out.println("\n");
+            }
+
+            rs.close();
+            stmt.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        } 
+    }
+}
